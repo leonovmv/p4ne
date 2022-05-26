5cc4548589840dc31d4815f4121c871aa260cbc8
leonovmv <mdp1rat3@gmail.com>
2022-05-26 15:02:19 +0300

    snmp test

--- Lab1.3/snmp_test.py
+++ Lab1.3/snmp_test.py
@@ -11,11 +11,12 @@ snmp_interfaces = ObjectIdentity("1.3.6.1.2.1.2.2.1.2")
 result = getCmd(snmp_engine, community_data, transport, context_data, ObjectType(snmp_system))
 result2 = nextCmd(snmp_engine, community_data, transport, context_data, ObjectType(snmp_interfaces), lexicographicMode=False)
 
-
+print("snmp_system")
 for e in result:
     for a in e[3]:
         print(a)
 
+print("snmp_interfaces")
 for e in result2:
     for a in e[3]:
         print(a)
