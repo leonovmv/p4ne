6e489d068e14821de2cd2b6125577e8648c4747e
leonovmv <mdp1rat3@gmail.com>
2022-05-26 12:16:43 +0300

    snmp test

---
+++ Lab1.3/snmp_test.py
@@ -0,0 +1,21 @@
+from pysnmp.hlapi import *
+
+snmp_engine = SnmpEngine()
+community_data = CommunityData("public", mpModel=0)
+transport = UdpTransportTarget(("10.31.70.107", 161))
+context_data = ContextData()
+
+snmp_system = ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)
+snmp_interfaces = ObjectIdentity("1.3.6.1.2.1.2.2.1.2")
+
+result = getCmd(snmp_engine, community_data, transport, context_data, ObjectType(snmp_system))
+result2 = nextCmd(snmp_engine, community_data, transport, context_data, ObjectType(snmp_interfaces), lexicographicMode=False)
+
+
+for e in result:
+    for a in e[3]:
+        print(a)
+
+for e in result2:
+    for a in e[3]:
+        print(a)
